c747e6a46e67e362150c841c84a33b8fbc486928
HDwivedi <hemaasheesh@gmail.com>
2020-03-21 13:47:54 -0400

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXSITS burgers_db;
+ CREATE DATABASE burgers_db;
+ USE burgers_db;
+
+ CREATE TABLE burgers(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(60),
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+ );
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers ( burger_name,devoured ) VALUES ('Happy meal burger', true);
+INSERT INTO burgers ( burger_name,devoured ) VALUES ('Cheese burger', true);
+INSERT INTO burgers ( burger_name,devoured ) VALUES ('Mach aloo tikki burger', true);
+SHOW TABLES;
+ SELECT * FROM burgers;
