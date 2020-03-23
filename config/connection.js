d60256b2701ce3bd649677b91f740d63797d6f99
HDwivedi <hemaasheesh@gmail.com>
2020-03-23 02:01:54 -0400

    add style

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 //set up mysql connection
-const mysql=require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE
-  });
-  
-  //make connection
-  connection.connect(function(err){
-      if(err) {
-          console.error("error connecting: "+err.stack);
-      }
-      console.log("connected as id "+connection.threadId);
-  });
+});
 
-  module.exports=connection;
+//make connection
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
