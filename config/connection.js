1f2978cd507189143c75cbee2955868f189b0d8c
HDwivedi <hemaasheesh@gmail.com>
2020-03-25 01:07:13 -0400

    added heroku jwsurl

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,20 @@
 //set up mysql connection
 const mysql = require("mysql");
+const dotenv = require("dotenv").config();
 
-const getConnection = () => {
-    if (process.env.JAWSDB_URL) {
-        return mysql.createConnection(process.env.JAWSDB_URL);
-    } else {
-        return mysql.createConnection({
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DATABASE
-        });
-    }
-};
+let connection;
 
-const connection = getConnection();
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE
+    });
+}
 
 //make connection
 connection.connect(function (err) {
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const methodOverride = require('method-override');
-const env = require("dotenv").config();
 
 const burgersController = require("./controllers/burgers_controller.js");
 
