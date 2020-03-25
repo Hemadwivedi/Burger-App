eacc1676908f4a86ebb2341008542006ba2d6f49
HDwivedi <hemaasheesh@gmail.com>
2020-03-25 00:13:24 -0400

    added heroku jwsurl

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 //set up mysql connection
 const mysql = require("mysql");
+if(process.env.JAWSDB_URL) {
+    connection=mysql.createConnection(process.env.JAWSDB_URL);
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE
-});
+}else{
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE
+    });
+};
 
 //make connection
 connection.connect(function (err) {
